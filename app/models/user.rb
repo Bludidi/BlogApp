1f63f349dc094c25a65146691db607dd94e841d0
Bludidi <bludidi5@gmail.com>
2023-02-11 22:20:27 +0200

    Fix rubocop linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
   after_save :most_recent_post
- 
+
   def most_recent_post
     posts.order(created_at: :desc).limit(3)
   end
