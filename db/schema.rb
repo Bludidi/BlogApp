e61de123979707f3204d141aee730b88fca8d07c
Bludidi <bludidi5@gmail.com>
2023-02-14 21:30:59 +0200

    Add user controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_11_185114) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_11_193827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "AuthorId"
     t.integer "PostId"
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
     t.index ["AuthorId"], name: "index_comments_on_AuthorId"
     t.index ["PostId"], name: "index_comments_on_PostId"
+    t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -32,9 +34,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_11_185114) do
     t.integer "AuthorId"
     t.integer "PostId"
     t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
     t.index ["AuthorId"], name: "index_likes_on_AuthorId"
     t.index ["PostId"], name: "index_likes_on_PostId"
     t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -45,7 +49,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_11_185114) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "AuthorId"
+    t.bigint "user_id", null: false
     t.index ["AuthorId"], name: "index_posts_on_AuthorId"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,6 +63,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_11_185114) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "users"
-  add_foreign_key "likes", "posts"
+  add_foreign_key "comments", "posts", column: "post_id"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts", column: "post_id"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
