3b5547e0b3b12ec8172bda32b5d262b7d3df4604
Bludidi <bludidi5@gmail.com>
2023-02-15 20:58:29 +0200

    Create model

--- db/migrate/20230211154837_add_id_to_comments.rb
+++
@@ -1,6 +0,0 @@
-class AddIdToComments < ActiveRecord::Migration[7.0]
-  def change
-    add_column :comments, :AuthorId, :integer
-    add_index :comments, :AuthorId
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_11_193827) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_165718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_11_193827) do
     t.datetime "updated_at", null: false
     t.integer "AuthorId"
     t.bigint "user_id", null: false
+    t.integer "author_id"
     t.index ["AuthorId"], name: "index_posts_on_AuthorId"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
@@ -63,9 +64,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_11_193827) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts", column: "post_id"
-  add_foreign_key "comments", "users", column: "author_id"
-  add_foreign_key "likes", "posts", column: "post_id"
-  add_foreign_key "likes", "users", column: "author_id"
-  add_foreign_key "posts", "users", column: "author_id"
+  add_foreign_key "comments", "posts", 
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
