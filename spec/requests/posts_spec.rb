795bd2dd3b5f0c1a3b8a4d904d43e46882b98214
Bludidi <bludidi5@gmail.com>
2023-02-20 17:12:23 +0200

    Add post rspec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -6,21 +6,21 @@ RSpec.describe "PostsController", type: :request do
     it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
-    it "renders 'index' tempalte" do
-      expect(response).to render_tempalte(:index)
+    it "renders 'index' template" do
+      expect(response).to render_template(:index)
     end
     it "renders 'index' body" do
       expect(response.body).to include('Showing all posts for user with id:')
     end
   end
 
-  describe 'GET posts_controller#show' do
-    before(:example) { get 'users/:user_id/posts/:post_id' }
+  describe "GET posts_controller#show" do
+    before(:example) { get 'http://localhost:3000/users/:user_id/posts/:id' }
     it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
-    it "renders 'show' tempalte" do
-      expect(response).to render_tempalte(:show)
+    it "renders 'show' template" do
+      expect(response).to render_template(:show)
     end
     it "renders 'index' body" do
       expect(response.body).to include('Showing posts with id:')
