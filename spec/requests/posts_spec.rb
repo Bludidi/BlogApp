633db7233d692bb1fdd7c29f924422f4ac87de4e
Bludidi <bludidi5@gmail.com>
2023-02-20 12:25:23 +0200

    Craete posts controller Rspec test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "PostsController", type: :request do
+  describe "GET posts_controller#index" do
+    before(:example) { get '/users/:user_id/posts' }
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'index' tempalte" do
+      expect(response).to render_tempalte(:index)
+    end
+    it "renders 'index' body" do
+      expect(response.body).to include('Showing all posts for user with id:')
+    end
+  end
+
+  describe 'GET posts_controller#show' do
+    before(:example) { get 'users/:user_id/posts/:post_id' }
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'show' tempalte" do
+      expect(response).to render_tempalte(:show)
+    end
+    it "renders 'index' body" do
+      expect(response.body).to include('Showing posts with id:')
+    end
   end
 end
