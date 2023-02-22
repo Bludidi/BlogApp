af95270ef1deab2455168477fb351af84fa8febd
Bludidi <bludidi5@gmail.com>
2023-02-22 23:06:35 +0200

    Add user controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,26 @@
 class PostsController < ApplicationController
   def index
-    @user_id = params[:user_id]
+    @user = User.find(params[:user_id])
+    @posts = Post.find(params[:user_id])
   end
 
   def show
-    @user_id = params[:user_id]
-    @post_id = params[:id]
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    user = current_user
+    @post = Post.new(
+      title: params[:title],
+      text: params[:text],
+      author_id: user.id
+    )
+    return unless @post.save
+    redirect_to user_posts_path
   end
 end
