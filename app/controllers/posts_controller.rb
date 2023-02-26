012fda1567f9c9232b713129b43340434a60c12a
Bludidi <bludidi5@gmail.com>
2023-02-26 09:28:03 +0200

    Add show erb context

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,6 @@ class PostsController < ApplicationController
 
   def show
     @user = User.find(params[:user_id])
-    @posts = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 end
