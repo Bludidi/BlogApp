90118a8a63a25e7c9d727c5c9a74136886bb1c8b
Bludidi <bludidi5@gmail.com>
2023-02-28 08:40:19 +0200

    Fix posts counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
   has_many :comments
   has_many :likes
-  after_save :update_posts_counter
+  after_create :update_posts_counter
   validates :title, presence: true
   validates :title, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
@@ -13,6 +13,7 @@ class Post < ApplicationRecord
   end
 
   def update_posts_counter
-    User.posts.increment!(:posts_counter)
+    author_id = author.id
+    User.increment_counter(:posts_counter, author_id)
   end
 end
