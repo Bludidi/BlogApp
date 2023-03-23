eef801d43b8751beabb7fd7dfc91197f6e42c8f3
Bludidi <bludidi5@gmail.com>
2023-03-23 23:04:27 +0200

    Fix likes_counter error

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,11 +2,11 @@ class LikesController < ApplicationController
   def create
     user = current_user
     @like = Like.new(
-      post_id: params[:post_id],
+      post: Post.find(params[:post_id]),
       author_id: user.id
     )
     return unless @like.save
 
-    redirect_to user_likes_path
+    redirect_to user_post_likes_path(@like.post_id, @like.author_id)
   end
 end
