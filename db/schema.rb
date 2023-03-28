875d66191ca161e4e59a95bac3bcb2f011cc1391
Brian Ludidi <bludidi5@gmail.com>
2023-03-28 05:57:54 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-  # These are extensions that must be enabled in order to support this database
+
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_165718) do# These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
