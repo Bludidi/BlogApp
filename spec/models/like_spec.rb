f4b522be464db139a61edbe2a960df1ea4563b08
Bludidi <bludidi5@gmail.com>
2023-03-29 14:33:27 +0200

    Create unit test for likes model

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  subject { Like.new(post_id: 9) }
+
+  before { subject.save }
+
+  it 'The post_id should be an integer' do 
+    subject.post_id = '15'
+    expect(subject).to_not be_valid
+  end
+end
