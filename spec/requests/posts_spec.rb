40a6b5edde1e1fed501a64e7871cfdc53ee52cbd
Michael Amponsah <amponsahmichaelkwame@gmail.com>
2023-04-19 11:44:47 +0000

    Add integration specs for Posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,29 +1,7 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "PostsController", type: :request do
-#   describe "GET posts_controller#index" do
-#     before(:example) { get '/users/:user_id/posts' }
-#     it 'is a success' do
-#       expect(response).to have_http_status(:ok)
-#     end
-#     it "renders 'index' template" do
-#       expect(response).to render_template(:index)
-#     end
-#     it "renders 'index' body" do
-#       expect(response.body).to include('Showing all posts for user with id:')
-#     end
-#   end
-
-#   describe "GET posts_controller#show" do
-#     before(:example) { get 'http://localhost:3000/users/:user_id/posts/:id' }
-#     it 'is a success' do
-#       expect(response).to have_http_status(:ok)
-#     end
-#     it "renders 'show' template" do
-#       expect(response).to render_template(:show)
-#     end
-#     it "renders 'index' body" do
-#       expect(response.body).to include('Showing posts with id:')
-#     end
-#   end
-# end
+RSpec.describe "Posts", type: :request do
+  describe "GET /index" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end
