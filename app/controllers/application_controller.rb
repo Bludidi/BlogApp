98afba50506e8f4d66699a420c8592119f630863
Bludidi <bludidi5@gmail.com>
2023-04-20 18:52:54 +0200

    Add validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email])
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :bio, :email, :password, :password_confirmation, :current_password)
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name]) 
   end
 end
