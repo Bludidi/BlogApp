bb420b7263fe0b16224eb5eec383b39a64044e4c
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-04-22 12:47:56 +0000

    Modify post model to fit app requirements

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
-  has_many :comments
-  has_many :likes
-  after_create :update_posts_counter
-  validates :title, presence: true
-  validates :title, length: { maximum: 250 }
+  has_many :comments, foreign_key: 'post_id', dependent: :destroy
+  has_many :likes, foreign_key: 'post_id', dependent: :destroy
+  after_save :update_posts_counter
+  validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
@@ -13,7 +12,6 @@ class Post < ApplicationRecord
   end
 
   def update_posts_counter
-    author_id = author.id
-    User.increment_counter(:posts_counter, author_id)
+    author.update(posts_counter.posts.count)
   end
 end
